f9c4a5f78b59ba0de4d5b966be00e9395c7e5a17
Simon Cole <simonccole1@gmail.com>
2023-08-06 13:24:17 -0400

    react query testing

--- components/FindActivityBox.tsx
+++ components/FindActivityBox.tsx
@@ -1,10 +1,19 @@
+import { useQuery } from "@tanstack/react-query"
 import { useState } from "react"
 
 export default function FindActivityBox() {
     const [inputState, setInputState] = useState<string>('')
     const handleClick = () => {
-        
+                
     }
+
+    const fetchTest = async () => {
+        const res = await fetch('/api/hello')
+        const data = await res.json()
+        return data
+    }
+
+    const { data, isLoading, error } = useQuery(['test'], fetchTest)
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setInputState(e.target.value)
@@ -15,6 +24,9 @@ export default function FindActivityBox() {
             <h3>Enter an activity ID in the search box below to generate a story</h3>
             <input type="text" onChange={handleInputChange} value={inputState} />
             <button onClick={handleClick} type="button">Generate Story</button>
+            {isLoading && <p>Loading...</p>}
+            {error && <p>Error</p>}
+            {data && <p>{data.name}</p>}
         </div>
     )
 }
